75461736b3ff38e9398669915e60a71dbee8d59d
unknown <tombirbeck47@gmail.com>
2022-08-21 09:54:18 +0100

    added state to handle double and triple words and toggle display

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -8,14 +8,30 @@ export default function Display(){
     const [displayWord, SetDisplayWord] = useState([])
     const [tripleScore, setTripleScore] = useState(false)
     const [doubleScore, setDoubleScore] = useState(false)
+    const [mode, setMode] = useState(1)
 
     const [score, SetScore] = useState(0)
 
-    function handleWordClick(displayWord){
-        SetScore(calculateDoubleScrabbleScore(displayWord))
+    function handleWordClick(displayWord, mode){
+        SetScore(calculateScrabbleScore(displayWord) * mode)
         SetDisplayWord([])
+        setMode(1)
+        setDoubleScore(false)
+        setTripleScore(false)
     }
   
+    function handleDoubleWordClick(){
+        setDoubleScore(true)
+        setTripleScore(false)
+        setMode(2)
+    }
+
+    function handleTripleWordClick(){
+        setTripleScore(true)
+        setDoubleScore(false)
+        setMode(3)
+    }
+
     console.log("current", displayWord)
     return (
         <WordContext.Provider value = {{displayWord, SetDisplayWord}}>
@@ -23,11 +39,11 @@ export default function Display(){
         <h1>{displayWord}</h1>
         {tripleScore ? <h3>Triple Word Score Active</h3> : null}
         {doubleScore ? <h3>Double Word Score Active</h3> : null}
-        <button onClick = {()=>{handleWordClick(displayWord)}}>Submit</button>
+        <button onClick = {()=>{handleWordClick(displayWord, mode)}}>Submit</button>
         <button>double letter</button>
         <button>triple letter</button>
-        <button onClick={() => {setDoubleScore(current => !current)}}>double word</button>
-        <button onClick={() => {setTripleScore(current => !current)}}>triple word</button>
+        <button onClick={handleDoubleWordClick}>double word</button>
+        <button onClick={handleTripleWordClick}>triple word</button>
         <h2> Word score {score} </h2>
         <ScrabbleButton/>
         </div>
