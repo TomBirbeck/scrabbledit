9ea19fde6f9f2779f6c0bfbe235a73104a4087b5
unknown <tombirbeck47@gmail.com>
2022-08-27 09:51:20 +0100

    added double and triple letter buttons to word display

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -9,6 +9,8 @@ export default function Display(){
     const [displayWord, SetDisplayWord] = useState([])
     const [tripleScore, setTripleScore] = useState(false)
     const [doubleScore, setDoubleScore] = useState(false)
+    const [doubleLetter, setDoubleLetter] = useState(false)
+    const [tripleLetter, setTripleLetter] = useState(false)
     const [mode, setMode] = useState(1)
     const [score, SetScore] = useState(0)
 
@@ -40,17 +42,18 @@ export default function Display(){
         setMode(3)
     }
 
-    console.log("current", displayWord)
+    console.log("double", doubleLetter)
+    console.log("triple", tripleLetter)
     return (
         <WordContext.Provider value = {{displayWord, SetDisplayWord}}>
         <div>
-        <h1>{displayWord.map((i) => { return <button id="display-button">{i}</button>})}</h1>
+        <h1 id="letter-layout">{displayWord.map((i) => { return <div id="letter-button-layout"><button id="display-button">{i}</button><button onClick={()=>{setDoubleLetter(!doubleLetter)}}>Double</button><button onClick={()=>{setTripleLetter(!tripleLetter)}}>Triple</button></div>})}</h1>
         {tripleScore ? <h3>Triple Word Score Active</h3> : null}
         {doubleScore ? <h3>Double Word Score Active</h3> : null}
         <button onClick = {()=>{handleWordClick(displayWord, mode)}}>Submit</button>
         <button onClick={() => {SetDisplayWord(handleDelete(displayWord))}}>remove letter</button>
-        <button>double letter</button>
-        <button>triple letter</button>
+        {/* <button>double letter</button> */}
+        {/* <button>triple letter</button> */}
         <button onClick={handleDoubleWordClick}>double word</button>
         <button onClick={handleTripleWordClick}>triple word</button>
         <h2> Word score {score} </h2>
