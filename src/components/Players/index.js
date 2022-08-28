b44cb2deb42a0e5cec3a4b7de6aa64fe5bbb192c
unknown <tombirbeck47@gmail.com>
2022-08-28 08:55:01 +0100

    fixed player table issue and make turn text conditionally render

--- src/components/Players/index.js
+++ src/components/Players/index.js
@@ -1,29 +1,17 @@
 import { useState } from "react"
 import "./players.css"
-import { WordContext } from "../Context";
+// import { WordContext } from "../Context";
 
 
 export default function Players(){
     const [players, Setplayers] = useState([]);
     const [player, Setplayer] = useState("");
     const [turn, SetTurn] = useState("");
-
-    console.log(WordContext.currentValue)
     
     function handleplayer1(e){
         e.preventDefault();
         Setplayer(e.target.value);
     }
-    
-    // function handleplayer2(e){
-    //     Setplayer(e.target.value)
-    // }
-    // function handleplayer3(e){
-    //     Setplayer(e.target.value)
-    // }
-    // function handleplayer4(e){
-    //     Setplayer(e.target.value)
-    // }
 
     function clickPlayer1(object){
     const playerstuff = players;
@@ -35,39 +23,15 @@ export default function Players(){
     function playerTurn(player){
         SetTurn(player.name)
     }
-    console.log("turn", turn)
-    // function clickPlayer2(object){
-    //     Setplayers(object[1].name = player)
-    // }
-    // function clickPlayer3(object){
-    //     Setplayers(object[2].name = player)
-    // }
-    // function clickPlayer4(object){
-    //     Setplayers(object[3].name = player)
-    // }
+    
     console.log("players", players)
     return (
         <div>
-        <h2> It's {turn}'s turn to play</h2>
+        <div>{turn.length > 0 ? <h2> It's {turn}'s turn to play</h2>: null}</div>
         <h3>Player input</h3>
         <input type = "text" onChange={handleplayer1} value = {player}></input><button onClick = {() => {clickPlayer1(player)}}>Submit</button>
-        {/* <input type = "text" onChange={handleplayer1} placeholder = "player 2"></input><button onClick = {() => {clickPlayer1(player)}}>Submit</button>
-        <input type = "text" onChange={handleplayer1} placeholder = "player 3"></input><button onClick = {() => {clickPlayer1(player)}}>Submit</button>
-        <input type = "text" onChange={handleplayer1} placeholder = "player 4"></input><button onClick = {() => {clickPlayer1(player)}}>Submit</button> */}
      <table>
-            <th>Player</th>
-            <th>Score</th>
-            {/* <th>{WordContext}</th> */}
-            {/* <tbody>
-            <td>{players.player1}</td>
-            <td>Score: 0</td>
-            <td>{players.player2}</td>
-            <td>Score: 0</td>
-            <td>{players.player3}</td>
-            <td>Score: 0</td>
-            <td>{players.player4}</td>
-            <td>Score: 0</td>
-            </tbody> */}
+        <thead><tr><td>Player</td><td>Score</td></tr></thead>
         <tbody>
         {players.map((val) => {
             return (<tr key = {players.length++}>
