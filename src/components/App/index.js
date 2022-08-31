6bcaf75583aefd3d22aedec6f655839ccde8ece8
unknown <tombirbeck47@gmail.com>
2022-08-31 20:26:09 +0100

    set player name entry to no longer allow empty field

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,10 @@
-// import Players from '../Players';
 import './App.css';
-// import {useState} from 'react'
 import { WordContext } from '../Context';
 import Display from '../Display';
 import Instructions from '../Instructions';
 
 function App() {
-// const [players, Setplayers] = useState([{player:"", score: 0}, {player:"", score: 0}, {player:"", score: 0}])
+
 
   return (
     <WordContext.Provider value={WordContext}>
@@ -15,8 +13,6 @@ function App() {
       <div id='title-container'><div id='title-banner'><h1 id='title'>Scrabbledit</h1></div></div>
        <h3 id='motto'>We can't trust Barry with the scores again</h3>
        <Display/>
-      {/* <Display playersNames={players} setPlayers = {Setplayers}/> */}
-      {/* <Players playersNames={players} setPlayers = {Setplayers}/> */}
     </div>
     </WordContext.Provider>
   );
