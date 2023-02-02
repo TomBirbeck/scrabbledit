ee6cf2005e94fb6d0f082ebf5529b1abb240b80a
unknown <tombirbeck47@gmail.com>
2023-02-02 15:49:56 +0000

    added tests for table headers and player buttons

--- src/components/Players/Players.test.js
+++ src/components/Players/Players.test.js
@@ -34,4 +34,43 @@ describe('player scoreboard tests', () => {
         const button4 = screen.getByText(/Player 4/);
         expect(button4).toBeInTheDocument()
     })
+
+    test('table headers render', () => {
+        const props = {
+            turn: 'Dave',
+            player1: {name: 'John', score: 0},
+            player2: {name: 'Simone', score: 0},
+            player3: {name: 'Claire', score: 0},
+            player4: {name: 'Greg', score: 0},
+            finalTiles: [0,0,0,0]
+        }
+        render(<Players turn={props.turn} player1={props.player1} player2={props.player2} player3={props.player3} player4={props.player4} finalTiles={props.finalTiles}/>);
+        const playerHeader = screen.getByText('Player');
+        expect(playerHeader).toBeInTheDocument();
+        const playerScore = screen.getByText('Score');
+        expect(playerScore).toBeInTheDocument();
+        const playerSpares = screen.getByText('Spare Tiles');
+        expect(playerSpares).toBeInTheDocument();
+        const playerFinal = screen.getByText('Final Score');
+        expect(playerFinal).toBeInTheDocument();
+    })
+    test('player buttons', () => {
+        const props = {
+            turn: 'Dave',
+            player1: {name: 'John', score: 0},
+            player2: {name: 'Simone', score: 0},
+            player3: {name: 'Claire', score: 0},
+            player4: {name: 'Greg', score: 0},
+            finalTiles: [0,0,0,0]
+        }
+        render(<Players turn={props.turn} player1={props.player1} player2={props.player2} player3={props.player3} player4={props.player4} finalTiles={props.finalTiles}/>);
+        const player1Button = screen.getByRole('button', {name: 'John'});
+        expect(player1Button).toBeInTheDocument()
+        const player2Button = screen.getByRole('button', {name: 'Simone'});
+        expect(player2Button).toBeInTheDocument()
+        const player3Button = screen.getByRole('button', {name: 'Claire'});
+        expect(player3Button).toBeInTheDocument()
+        const player4Button = screen.getByRole('button', {name: 'Greg'});
+        expect(player4Button).toBeInTheDocument()
+    })
 })
