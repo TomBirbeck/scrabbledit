abcc1c987223e4471ff39e7394d8da1448b3725e
unknown <tombirbeck47@gmail.com>
2023-02-08 17:10:49 +0000

    added tests for triple, double triple and triple triple mode

--- src/components/ScoreModeCalc/index.test.js
+++ src/components/ScoreModeCalc/index.test.js
@@ -32,7 +32,7 @@ describe('tests for score mode calc function in double mode', () => {
         [5, 'double', 10],
         [9, 'double', 18],
     ])
-    (`When given a score and 'double' scoremodecalc function returns the same score`, (score, mode, expected) =>{
+    (`When given a score and 'double' scoremodecalc function returns the score doubled`, (score, mode, expected) =>{
         const actual = ScoreModeCalc(score, mode);
        expect(actual).toBe(expected);
    });
@@ -51,7 +51,65 @@ describe('tests for score mode calc function in doubleDouble mode', () => {
         [5, 'doubleDouble', 20],
         [9, 'doubleDouble', 36],
     ])
-    (`When given a score and 'doubleDouble' scoremodecalc function returns the same score`, (score, mode, expected) =>{
+    (`When given a score and 'doubleDouble' scoremodecalc function returns the score doubled twice`, (score, mode, expected) =>{
+        const actual = ScoreModeCalc(score, mode);
+       expect(actual).toBe(expected);
+   });
+})
+
+describe('tests for score mode calc function in triple mode', () => {
+    test.each([
+        [10, 'triple', 30],
+        [15, 'triple', 45],
+        [20, 'triple', 60],
+        [30, 'triple', 90],
+        [40, 'triple', 120],
+        [55, 'triple', 165],
+        [8, 'triple', 24],
+        [3, 'triple', 9],
+        [5, 'triple', 15],
+        [9, 'triple', 27],
+    ])
+    (`When given a score and 'triple' scoremodecalc function returns the score tripled`, (score, mode, expected) =>{
+        const actual = ScoreModeCalc(score, mode);
+       expect(actual).toBe(expected);
+   });
+}
+)
+describe('tests for score mode calc function in doubleTriple mode', () => {
+    test.each([
+        [10, 'doubleTriple', 90],
+        [15, 'doubleTriple', 135],
+        [20, 'doubleTriple', 180],
+        [30, 'doubleTriple', 270],
+        [40, 'doubleTriple', 360],
+        [55, 'doubleTriple', 495],
+        [8, 'doubleTriple', 72],
+        [3, 'doubleTriple', 27],
+        [5, 'doubleTriple', 45],
+        [9, 'doubleTriple', 81],
+    ])
+    (`When given a score and 'triple' scoremodecalc function returns the score tripled twice`, (score, mode, expected) =>{
+        const actual = ScoreModeCalc(score, mode);
+       expect(actual).toBe(expected);
+   });
+}
+)
+
+describe('tests for score mode calc function in tripleTriple mode', () => {
+    test.each([
+        [10, 'tripleTriple', 270],
+        [15, 'tripleTriple', 405],
+        [20, 'tripleTriple', 540],
+        [30, 'tripleTriple', 810],
+        [40, 'tripleTriple', 1080],
+        [55, 'tripleTriple', 1485],
+        [8, 'tripleTriple', 216],
+        [3, 'tripleTriple', 81],
+        [5, 'tripleTriple', 135],
+        [9, 'tripleTriple', 243],
+    ])
+    (`When given a score and 'tripleTriple' scoremodecalc function returns the same score`, (score, mode, expected) =>{
         const actual = ScoreModeCalc(score, mode);
        expect(actual).toBe(expected);
    });
